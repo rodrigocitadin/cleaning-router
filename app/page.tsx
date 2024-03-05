6e7533f538c7398b440ce0823c0ec32e9c3cef41
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-05 18:50:22 -0300

    wip on clients list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ListInputs from "@/app/components/ListInputs";
-import PlusIcon from "./icons/PlusIcon";
+import PlusIcon from "@/app/icons/PlusIcon";
+import MinusIcon from "@/app/icons/MinusIcon";
 
 export default function Home() {
   return (
@@ -7,23 +8,27 @@ export default function Home() {
       <div className="flex gap-28 align-top justify-center mt-24">
         <div className="flex flex-col bg-gray-800 rounded-md p-4">
           <ListInputs />
-          <div className="flex flex-col mt-8 gap-2">
-            <div className="flex justify-between align-middle">
-              <span className="text-lg">George Josh</span>
-              <button><PlusIcon/></button>
-            </div>
-            <div className="flex justify-between align-middle">
-              <span className="text-lg">John Wick</span>
-              <button><PlusIcon/></button>
-            </div>
-            <div className="flex justify-between align-middle">
+          <div className="flex flex-col mt-8 gap-2 align-middle">
+            <div className="flex justify-between">
               <span className="text-lg">Dominic Toretto</span>
-              <button><PlusIcon/></button>
+              <button className="self-center"><MinusIcon /></button>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-lg">George John</span>
+              <button className="self-center"><PlusIcon /></button>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-lg">John Wick</span>
+              <button className="self-center"><PlusIcon /></button>
             </div>
           </div>
         </div>
-        <div className="flex flex-col bg-gray-800 p-4 rounded-md">
+        <div className="flex flex-col gap-4 bg-gray-800 p-4 rounded-md">
           <h2 className="text-xl">SELECIONADOS</h2>
+          <div className="flex justify-between">
+            <span className="text-lg">Dominic Toretto</span>
+            <button className="self-center"><MinusIcon /></button>
+          </div>
         </div>
       </div>
     </main>
