0982be7d0fcccb8f82377ca6abde28c36ec9bdba
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-05 21:59:38 -0300

    switch clients to a separated file

---
+++ app/utils/clients.ts
@@ -0,0 +1,20 @@
+  export const clients: Client[] = [
+    {
+      id: 1,
+      name: "Dominic Toretto",
+      phone: "49999119911",
+      selected: true
+    },
+    {
+      id: 2,
+      name: "George Jogn",
+      phone: "49999119911",
+      selected: true
+    },
+    {
+      id: 3,
+      name: "Keanu Reeves",
+      phone: "49999119911",
+      selected: false
+    }
+  ]
