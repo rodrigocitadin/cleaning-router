f055b26e135832cb44b09019dbabb5ff93989d65
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-11 14:10:46 -0300

    wip

--- app/components/ClientList.tsx
+++ app/components/ClientList.tsx
@@ -1,3 +1,6 @@
+import SearchIcon from "@/app/icons/SearchIcon";
+import CreateIcon from "@/app/icons/CreateIcon";
+import TrashIcon from "@/app/icons/TrashIcon";
 import ClientContainer from "@/app/components/ClientContainer"
 import { useClients } from "@/app/components/ClientContext"
 
@@ -5,15 +8,23 @@ export default function ClientList() {
   const [clients] = useClients();
 
   return (
-    <div className="flex flex-col mt-8 gap-2 align-middle">
-      {
-        clients.map(client=> (
-          <ClientContainer
-            key={client.id}
-            client={client}
-          />
-        ))
-      }
+    <div className="flex flex-col bg-gray-800 rounded-md p-4">
+      <div className="flex gap-8">
+        <input className="h-8 my-auto bg-transparent border-b-2" />
+        <button><SearchIcon /></button>
+        <button><CreateIcon /></button>
+        <button><TrashIcon /></button>
+      </div>
+      <div className="flex flex-col mt-8 gap-2 align-middle">
+        {
+          clients.map(client => (
+            <ClientContainer
+              key={client.id}
+              client={client}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import ListInputs from "@/app/components/ListInputs";
 import ClientList from "@/app/components/ClientList";
 import SelectedList from "@/app/components/SelectedList";
 import { ClientsProvider } from "@/app/components/ClientContext";
@@ -11,10 +10,7 @@ export default function Home() {
     <main>
       <ClientsProvider clients={clientsDefault}>
         <div className="flex gap-16 items-start justify-center mt-24">
-          <div className="flex flex-col bg-gray-800 rounded-md p-4">
-            <ListInputs />
-            <ClientList />
-          </div>
+          <ClientList />
           <SelectedList />
         </div>
       </ClientsProvider>
