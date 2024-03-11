4e57c9e0c886ff8a7e595ef5e6ae3ecc1a589f03
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-11 16:44:44 -0300

    wip

--- app/components/ClientList.tsx
+++ app/components/ClientList.tsx
@@ -3,26 +3,36 @@ import CreateIcon from "@/app/icons/CreateIcon";
 import TrashIcon from "@/app/icons/TrashIcon";
 import ClientContainer from "@/app/components/ClientContainer"
 import { useClients } from "@/app/components/ClientContext"
+import { useState } from "react";
 
 export default function ClientList() {
+  const [searchValue, setSearchValue] = useState('');
   const [clients] = useClients();
 
   return (
     <div className="flex flex-col bg-gray-800 rounded-md p-4">
       <div className="flex gap-8">
-        <input className="h-8 my-auto bg-transparent border-b-2" />
+        <input
+          type="text"
+          value={searchValue}
+          onChange={e => setSearchValue(e.target.value)}
+          placeholder="Search a name or number"
+          className="h-8 my-auto bg-transparent border-b-2"
+        />
         <button><SearchIcon /></button>
         <button><CreateIcon /></button>
         <button><TrashIcon /></button>
       </div>
       <div className="flex flex-col mt-8 gap-2 align-middle">
         {
-          clients.map(client => (
-            <ClientContainer
-              key={client.id}
-              client={client}
-            />
-          ))
+          searchValue.trim()
+            ? <h1>oi</h1>
+            : clients.map(client => (
+              <ClientContainer
+                key={client.id}
+                client={client}
+              />
+            ))
         }
       </div>
     </div>
