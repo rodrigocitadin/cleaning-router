bca22cff51b5e8fbd2a95400623c8b256eb74c79
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-11 13:29:00 -0300

    remove selected clients

--- app/components/SelectedContainer.tsx
+++ app/components/SelectedContainer.tsx
@@ -1,12 +1,30 @@
 import MinusIcon from "@/app/icons/MinusIcon";
+import { useClients } from "@/app/components/ClientContext";
 
 export default function SelectedContainer({ client }: {
   client: Client,
 }) {
+  const [clients, setClients] = useClients();
+
+  function handleClick() {
+    const updatedClient: Client = {
+      ...client,
+      selected: false
+    }
+    const updatedClients = clients.map(c => c.id === client.id ? updatedClient : c);
+
+    setClients(updatedClients);
+  }
+
   return (
     <div className="flex justify-between">
       <span className="text-lg">{client.name}</span>
-      <button className="self-center"><MinusIcon /></button>
+      <button
+        className="self-center"
+        onClick={handleClick}
+      >
+        <MinusIcon />
+      </button>
     </div>
   )
 }
