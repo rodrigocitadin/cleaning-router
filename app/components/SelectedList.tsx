b0175b089542a886b0af15adf5764b3183df07ec
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-12 17:08:11 -0300

    wip

--- app/components/SelectedList.tsx
+++ app/components/SelectedList.tsx
@@ -5,16 +5,18 @@ export default function SelectedList() {
   const [clients] = useClients();
 
   return (
-    <div className="flex flex-col gap-2 bg-gray-800 p-4 rounded-md min-w-64">
-      <h2 className="text-xl text-center mb-4">SELECTED</h2>
-      {
-        clients.some(c => c.selected)
-          ? clients.map(client => client.selected
-            ? <SelectedContainer key={client.id} client={client} />
-            : null
-          )
-          : <span className="text-center italic">No customers selected</span>
-      }
+    <div className="flex flex-col bg-gray-800 p-4 rounded-md min-w-64">
+      <h2 className="text-2xl text-center">SELECTED</h2>
+      <div className="mt-8">
+        {
+          clients.some(c => c.selected)
+            ? clients.map(client => client.selected
+              ? <SelectedContainer key={client.id} client={client} />
+              : null
+            )
+            : <span className="block m-[-1.5rem] text-center italic">No customers selected</span>
+        }
+      </div>
     </div>
   )
 }
