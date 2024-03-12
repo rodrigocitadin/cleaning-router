7af6ec5c3029cfc2faa871dc147e9ca37be76a3c
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-12 09:45:29 -0300

    delete selected users

--- app/components/ClientList.tsx
+++ app/components/ClientList.tsx
@@ -3,10 +3,15 @@ import TrashIcon from "@/app/icons/TrashIcon";
 import ClientContainer from "@/app/components/ClientContainer"
 import { useClients } from "@/app/components/ClientContext"
 import { useState } from "react";
+import Link from "next/link";
 
 export default function ClientList() {
   const [searchValue, setSearchValue] = useState('');
-  const [clients] = useClients();
+  const [clients, setClients] = useClients();
+
+  function deleteSelected() {
+    setClients(clients.filter(c => !c.selected));
+  }
 
   return (
     <div className="flex flex-col bg-gray-800 rounded-md p-4">
@@ -18,8 +23,8 @@ export default function ClientList() {
           placeholder="Search a name..."
           className="h-8 w-64 my-auto bg-transparent border-b-2"
         />
-        <button><CreateIcon /></button>
-        <button><TrashIcon /></button>
+        <Link className="self-center" href="/create"><CreateIcon /></Link>
+        <button onClick={deleteSelected}><TrashIcon /></button>
       </div>
       <div className="flex flex-col mt-8 gap-2 align-middle">
         {
