80b5da6d6cdd8afca50e4e2e80ec068594fc6ec8
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-11 21:35:50 -0300

    show message if no clients selected

--- app/components/SelectedList.tsx
+++ app/components/SelectedList.tsx
@@ -8,11 +8,12 @@ export default function SelectedList() {
     <div className="flex flex-col gap-2 bg-gray-800 p-4 rounded-md min-w-64">
       <h2 className="text-xl text-center mb-4">SELECIONADOS</h2>
       {
-        clients.map(client => {
-          return client.selected
+        clients.some(c => c.selected)
+          ? clients.map(client => client.selected
             ? <SelectedContainer key={client.id} client={client} />
             : null
-        })
+          )
+          : <span className="text-center italic">No customers selected</span>
       }
     </div>
   )
