6efe88936750d0a5ef5cc587e919d5d6315b96a4
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-12 14:31:47 -0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,12 @@
 
 import ClientList from "@/app/components/ClientList";
 import SelectedList from "@/app/components/SelectedList";
-import { ClientsProvider } from "@/app/components/ClientContext";
-import { clientsDefault } from "@/app/utils/clients";
 
 export default function Home() {
   return (
-    <main>
-      <ClientsProvider clients={clientsDefault}>
-        <div className="flex gap-16 items-start justify-center mt-24">
-          <ClientList />
-          <SelectedList />
-        </div>
-      </ClientsProvider>
+    <main className="flex gap-16 items-start justify-center mt-24">
+      <ClientList />
+      <SelectedList />
     </main >
   );
 }
